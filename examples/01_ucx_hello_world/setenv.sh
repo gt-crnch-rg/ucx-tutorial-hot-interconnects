4657d0140998031cb9273bac8c3075117c285e5a
Jeff Young <jyoung9@gatech.edu>
2021-08-20 10:26:45 -0400

    Cleaning up example 1

--- examples/01_ucx_hello_world/setenv.sh
+++ examples/01_ucx_hello_world/setenv.sh
@@ -2,5 +2,10 @@
 # This script helps to set up your path to compile and run UCX examples
 # Usage: source setenv.sh
 
-export UCX_ROOT=/opt/ucx-1.10.1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${UCX_ROOT}/lib
+#You can install UCX using Anaconda, Spack, or from source
+#For example with Anaconda: 
+#export UCX_PATH=$(HOME)/local/anaconda3/envs/shmem-py/
+
+#Here we assume that you have set your path from a source build - this needs to be changed for your local installation
+export UCX_PATH=/opt/ucx-1.10.1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${UCX_PATH}/lib
