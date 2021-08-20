5f4fb1c6df9c6a773b1b32d65558823165a1aee3
Jeff Young <jyoung9@gatech.edu>
2021-08-20 10:33:29 -0400

    Updated setenv script

--- examples/setenv_ucx.sh
+++ examples/setenv_ucx.sh
@@ -7,7 +7,7 @@
 #export UCX_PATH=$(HOME)/local/anaconda3/envs/shmem-py/
 
 #With Miniconda with environment named ucx_gpu
-#export UCX_PATH=${HOME}/%userprofile%.condaenvs/ucx_gpu/bin/
+#export UCX_PATH=${HOME}/%userprofile%.condaenvs/ucx_gpu/
 
 #Here we assume that you have set your path from a source build - this needs to be changed for your local installation
 export UCX_PATH=/opt/ucx-1.10.1
