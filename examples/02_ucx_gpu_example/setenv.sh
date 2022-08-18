63d0a8954414fa6f630b2762f97f79b7c009da2b
Jeffrey Young <jyoung3131@users.noreply.github.com>
2022-08-18 09:26:18 -0400

    Removing setenv - check top-level

--- examples/02_ucx_gpu_example/setenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script helps to set up your path to compile and run UCX examples
-# Usage: source setenv.sh
-
-export UCX_ROOT=/opt/ucx-1.10.1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${UCX_ROOT}/lib
