e2561e087eb53e3b54c8d6c6d9882f730ed5405c
Jeff Young <jyoung9@gatech.edu>
2022-08-18 20:22:42 -0400

    Updated UCX installation script

--- examples/install_ucx.sh
+++ examples/install_ucx.sh
@@ -10,16 +10,32 @@ INSTALL_PATH=/opt/ucx-$UCX_VERSION
 install_dependencies()
 {
 	printf "Installing dependencies"
+	sudo apt install nvidia-cuda-toolkit
 }
 
+#Build from source without CUDA support
 install_ucx_source()
 {
 	wget https://github.com/openucx/ucx/releases/download/v${UCX_VERSION}/ucx-${UCX_VERSION}.tar.gz
 	tar xzf ucx-${UCX_VERSION}.tar.gz
 	cd ucx-${UCX_VERSION}
+	
+	#Configure and build UCX from source
 	mkdir build && cd build
-	../configure --prefix=$INSTALL_PATH
+	./configure --prefix=$INSTALL_PATH
 	make -j4 && make install
+
+	#Clean up the build directory and tarball
+	cd .. && rm -rf ucx-${UCX_VERSION}
 }
 
+install_ucx_conda_gpu()
+{
+
+}
+
+#Use this to install UCX from source without CUDA support 
 install_ucx_source
+
+#Use this to install UCX with miniConda and GPU support
+install_ucx_conda_gpu
